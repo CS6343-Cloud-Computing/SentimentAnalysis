4910b7c29c24e528ad6b7297a985114508990289
sjchoure <sourabchoure98@gmail.com>
2022-10-12 11:06:45 -0500

    final changes for demo2

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from kafka import KafkaConsumer
 from kafka import KafkaProducer
+from kafka.structs import TopicPartition
 from textblob import TextBlob
 import os
 
 
 kafkaServer = os.environ.get('KafkaServer')
-
+containerName = os.environ.get('ContainerName')
 
 import nltk
 nltk.download('stopwords')
@@ -15,6 +16,8 @@ import re
 
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 nltk.download('vader_lexicon')
+
+
 sentiments = SentimentIntensityAnalyzer()
 stop_words = set(stopwords.words('english'))
 def clean_tweet(sentence):
@@ -44,22 +47,26 @@ def sentiment_analyser(tweet_str):
 
 
 
-
+print(kafkaServer.split(","))
 consumer = KafkaConsumer(
  bootstrap_servers=kafkaServer, api_version=(0,11,5), auto_offset_reset='earliest', group_id=None
 )
-consumer.subscribe(topics="abcd")
+consumer.subscribe(topics=containerName)
 
-producer = KafkaProducer(bootstrap_servers=[kafkaServer],api_version=(0,11,5))
-def produce_sentiment(topic, key, value):
-    producer.send( topic,  key = key, value = str(value).encode('utf-8'))
+producer = KafkaProducer(bootstrap_servers=kafkaServer,api_version=(0,11,5))
+def produce_sentiment(topic, key, value, headers):
+    producer.send( topic,  key = key, value = str(value).encode('utf-8'), headers=headers)
 
 
 
 for msg in consumer:
-    print(msg)
-    temp_tweet = clean_tweet(str(msg))
-    str_tweet = ' '.join(str(t) for t in temp_tweet)
-    val = sentiment_analyser(str_tweet)
-    print(val)
-    produce_sentiment("def", msg.key, str(val))
+	print(msg.headers)
+	steps = msg.headers[0][1].decode("utf-8").split("<-->")
+	pointer = 1
+	# int(msg.headers[1][1].decode("utf-8"))
+	temp_tweet = clean_tweet(str(msg))
+	str_tweet = ' '.join(str(t) for t in temp_tweet)
+	val = sentiment_analyser(str_tweet)
+	print(val)
+	outtopic =  "output" if pointer + 1 >= len(steps) else steps[pointer+1]
+	produce_sentiment(outtopic, msg.key, str(val), [('flow', "<-->".join(steps).encode()), ('pointer', str(pointer+1).encode())])
